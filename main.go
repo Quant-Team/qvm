dcf3080cf34bc0ff9a6d91212097970a44eedc48
Alexandr Kozlenkov <sah4ez32@gmail.com>
2019-09-24 22:40:07 +0300

    Added deploy stage and measurement function

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("I'm virtual qubit")
+	env := os.Getenv("SECRET")
+	fmt.Println(env)
+
+	fmt.Println("Measure me ^^")
+
+	Measure()
+}
+
+func Measure() {
+	rand.Seed(time.Now().UnixNano())
+	r := rand.Float32()
+	fmt.Printf("%f\n", r)
 }
